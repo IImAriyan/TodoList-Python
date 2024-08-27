53282bc3eb93d192cda373ea7068e1d9a0759837
MRHidden100 <adibariyanesmaeilzadeh@gmail.com>
2024-08-27 22:37:14 +0330

    Create Add Todos  Function

--- main.py
+++ main.py
@@ -29,9 +29,33 @@ def Todoslist():
     cursor.execute("SELECT * FROM todos");
     result = cursor.fetchall()
     todos = []
+
     for x in result:
          todos.append( {"title":x[1],"description":x[2],"id":x[0]})
+
     return flask.jsonify(todos)
+
+
+# ADD Todo Function
+@app.route('/api/Todo/add', methods=['POST'])
+def AddTodo():
+
+    # Get Body
+    data = flask.request.json
+
+    if (data.get('title') == None or data.get('description') == None ):
+        flask.request.status_code = 400
+        return flask.jsonify({"message": "Missing data","statusCode":400})
+    else:
+
+        title = data.get('title')
+        description = data.get('description')
+
+        # Add Todo In Database
+        cursor.execute("INSERT INTO todos (title, description) VALUES (%s, %s)", (title, description))
+
+        return flask.jsonify({"message": "Todo Successfully Added","statusCode":200})
+
 
 
 if (__name__ == '__main__'):
